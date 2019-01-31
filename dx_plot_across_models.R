2228a436b060800f96a50a09b9d1b0e8c529d3d5
David Wong <mingtak.wong1986@gmail.com>
2019-01-31 17:17:49 +0800

    this branch focus on LM object temporary (maybe meaningless to plot residual without transformation (i.e.: glm) graph function is added

--- dx_plot_across_models.R
+++ dx_plot_across_models.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 ###### 1. A function to extract fitted values and residuals across models 
 ##        Input:  model(or models), and some control parameter
 ##        Output: A data frame (tall shape) for plot
@@ -7,10 +8,10 @@
 ##        Output: ggplot
 
 library(ggplot2)
- 
+
 ext_from_mod <- function(...){
   mod_name_list <- vapply(match.call(expand.dots = F)$..., deparse, character(1))
-  if (all(sapply(mod_name_list, function(x) {'lm' %in% class(get(x)) })) == FALSE){
+  if (all(sapply(mod_name_list, function(x) {all(class(get(x)) == 'lm') })) == FALSE){
     stop('input is not in lm class')
   }
   
@@ -30,10 +31,10 @@ ext_from_mod <- function(...){
     # print(tmp_formula)
     y_applied <- tmp_formula[[1]]
     regMod_applied <- tmp_formula[[2]]
-
+    
     c(data_applied, y_applied, regMod_applied)
-    }
-    )
+  }
+  )
   dimnames(check_list) <- list(c('dataUsed', 'yUsed', 'regModUsed'),
                                c('m1', 'm2'))
   check_list <- data.frame(check_list)
@@ -62,6 +63,34 @@ ext_from_mod <- function(...){
   # tail(df_group)
 } 
 
+
+diag_plot <- function(consolidated_df, is_fitted_Y = TRUE, is_loess = TRUE){
+  if (is_loess == TRUE) {
+    fit_type <- 'loess'
+  } else {
+    fit_type <- 'lm'
+  }
+  # if (is_fitted_Y == TRUE) {
+  #   # x_opt <- as.name('fit_X')
+  #   x_opt <- 'fit_X'
+  # 
+  # } else {
+  #   # x_opt <- as.name('obs_ord_X')
+  #   x_opt <- 'obs_ord_X'
+  # 
+  # }
+  gg_1 <- ggplot(data = consolidated_df, aes(y = res_Y, shape = model_X, col = model_X))
+  
+  if (is_fitted_Y == TRUE) {
+    gg_1 + geom_point(aes(x = fit_X)) + 
+      geom_smooth(aes(x = fit_X), method = fit_type)
+  } else {
+    gg_1 + geom_point(aes(x = obs_ord_X)) + 
+      geom_smooth(aes(x = obs_ord_X), method = fit_type)
+  }
+}
+
+
 # ------------------------------------------------------------------------------
 # str(mtcars)
 mod_1 <- lm(mpg ~ wt, data = mtcars)
@@ -71,18 +100,33 @@ mod_2 <- update(mod_1, . ~ . + hp)
 ext_from_mod(mod_1, mod_2) # Test 1: valid input (lm) generate expected output?
 
 # install.packages('titanic')
-library(titanic)
+# library(titanic)
 # objects('package:titanic')
 # str(titanic_train)
-library(dplyr)
-titanic <- as.data.frame(titanic_train) %>% mutate(Sex = as.factor(Sex)) 
-mod_3 <- glm(Survived ~ Sex*Age,
-             family=binomial(link='logit'),data=titanic)
-mod_4 <- glm(Survived ~ Sex,
-               family=binomial(link='logit'),data=titanic)
-mod_5 <- glm(Sex ~ Survived,
-             family=binomial(link='logit'),data=titanic)
+# library(dplyr)
+# titanic <- as.data.frame(titanic_train) %>% mutate(Sex = as.factor(Sex)) 
+# mod_3 <- glm(Survived ~ Sex*Age,
+#              family=binomial(link='logit'),data=titanic)
+# mod_4 <- glm(Survived ~ Sex,
+#              family=binomial(link='logit'),data=titanic)
+# mod_5 <- glm(Sex ~ Survived,
+#              family=binomial(link='logit'),data=titanic)
 # summary(mod_3);summary(mod_4)
-ext_from_mod(mod_3, mod_4) # Test 2: valid input (glm) generate expected output?
-ext_from_mod(mod_1, mod_4) # Test 3: Same dataUsed across the model?
-ext_from_mod(mod_4, mod_5) # Test 4: Same predictors across the model?
+# ext_from_mod(mod_3, mod_4) # Test 2: valid input (glm) generate expected output?
+# ext_from_mod(mod_1, mod_4) # Test 3: Same dataUsed across the model?
+# ext_from_mod(mod_4, mod_5) # Test 4: Same predictors across the model?
+
+# ------------------------------------------------------------------------------
+consolidated_df_1 <- ext_from_mod(mod_1, mod_2)
+# consolidated_df_2 <- ext_from_mod(mod_3, mod_4)
+diag_plot(consolidated_df_1)
+diag_plot(consolidated_df_1, is_fitted_Y = TRUE, is_loess = TRUE) # Test 1: valid input (lm) generate expected output?
+diag_plot(consolidated_df_1, is_fitted_Y = FALSE, is_loess = TRUE) # Test 1: valid input (lm) generate expected output?
+diag_plot(consolidated_df_1, is_fitted_Y = TRUE, is_loess = FALSE) # Test 1: valid input (lm) generate expected output?
+diag_plot(consolidated_df_1, is_fitted_Y = FALSE, is_loess = FALSE) # Test 1: valid input (lm) generate expected output?
+
+# diag_plot(consolidated_df_2, is_fitted_Y = TRUE, is_loess = TRUE) # Test 1: valid input (lm) generate expected output?
+# diag_plot(consolidated_df_2, is_fitted_Y = FALSE, is_loess = TRUE) # Test 1: valid input (lm) generate expected output?
+# diag_plot(consolidated_df_2, is_fitted_Y = TRUE, is_loess = FALSE) # Test 1: valid input (lm) generate expected output?
+# diag_plot(consolidated_df_2, is_fitted_Y = FALSE, is_loess = FALSE) # Test 1: valid input (lm) generate expected output?
+# 
